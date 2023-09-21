8b6aa56c1d56ca51b8061bd6b0eaaff5d3ee05a5
Bisaso Ronald <bronnie313@gmail.com>
2023-09-21 18:10:41 +0300

    add unit tests for user and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :icome_transactions_categories
   has_many :icome_transactions, through: :icome_transactions_categories
+
+  validates :name, :icon, presence: true
 end
