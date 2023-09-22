7a1e7632e4e300a656a196ea16014d993419d2c9
Bisaso Ronald <bronnie313@gmail.com>
2023-09-22 16:23:08 +0300

    fix rubocop errors

--- spec/views/icome_transactions_spec.rb
+++ spec/views/icome_transactions_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Transaction_entry', type: :view do
   include Capybara::DSL
 
   before do
-    @user = User.create(name: 'ronnie', email: 'ronnie@gmail.com', password: 123456)
+    @user = User.create(name: 'ronnie', email: 'ronnie@gmail.com', password: 123_456)
     sign_in @user
     @category = @user.categories.create(name: 'gym', icon: 'gym.jpg')
     @icome_transaction = @category.icome_transactions.create(name: 'gloves', amount: 120, user_id: @user.id)
